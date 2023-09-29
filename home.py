e19cb51e251b6c710c5a82f6840ca44ecb4fe07d
Dario Z <dario.zignale@yahoo.com>
2023-09-29 21:32:40 +0100

    contact me page added

--- home.py
+++ home.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas
 
-
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -23,8 +22,7 @@ with col2:
 
 st.write("Below you can find some of the apps i have built in Python. Feel free to contact me!")
 
-
-col3,empty_col, col4 = st.columns([1.5,0.5,1.5])
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
